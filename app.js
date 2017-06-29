cadd3cf13334066cdabaf27a07b3be9f3308bb50
ben scarborough <benscarborough@scarflix.com>
2017-06-29 13:53:56 -0400

    I think it's done

--- app.js
+++ app.js
@@ -18,12 +18,12 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "mustache");
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(validator());
 
 app.use(routes);
 
-app.listen(3000, function(req, res) {
+app.listen(3000, function (req, res) {
     console.log("Hello world.")
 });
 
--- routes/router.js
+++ routes/router.js
@@ -2,21 +2,29 @@ const express = require('express');
 const router = express.Router();
 const models = require('../models');
 
-router.get("/", function(req, res) {
-    res.render('../views/todo')
+router.get("/", function (req, res) {
+    models.tasks.findAll().then(function (todo) {
+        res.render('../views/index', { myTasks: todo });
+    })
 });
 
-router.post('/', function(req, res) {
-    
+router.post('/', function (req, res) {
     var todo = models.tasks.build({
-  task: req.body.taskName, 
-  complete: false
-});
-todo.save().then(function(){
-    res.redirect("/");
-})
+        task: req.body.taskName,
+        complete: false
+    });
+    todo.save().then(function (todo) {
+        res.redirect("/");
+    })
 })
 
+router.get('/delete/:id', function (req, res) {
+    models.tasks.findById(req.params.id).then(function (task) {
+        task.destroy().then(function () {
+            res.redirect('/');
+        });
+    });
+})
 
 module.exports = router;
 
